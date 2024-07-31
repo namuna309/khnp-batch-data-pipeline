88b063ee8f491d143a382162df6bfc91a0f42343
namuna309 <nyah309@gmail.com>
2024-07-31 15:56:19 +0900

    spark 데이터 처리 후 s3 저장 기능 구현 및 raw데이터 수집 불가시 최근 파일 복사 기능 추가

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ import requests  # HTTP 요청을 보내기 위한 라이브러리
 from time import sleep  # 일정 시간 대기하기 위한 라이브러리
 import pandas as pd  # 데이터 처리 및 분석을 위한 라이브러리
 from urllib.parse import unquote  # URL 디코딩을 위한 라이브러리
-from datetime import datetime  # 날짜 및 시간 처리를 위한 라이브러리
+from datetime import datetime, timedelta  # 날짜 및 시간 처리를 위한 라이브러리
 from io import StringIO  # 문자열을 파일처럼 다루기 위한 라이브러리
 import boto3  # AWS SDK for Python
 import concurrent.futures
@@ -56,7 +56,7 @@ class DataCrawler:
     # 데이터를 S3에 저장하는 메서드
     def save_to_s3(self, df, file_path):
         now = datetime.now().strftime('%Y-%m-%d_%H')  # 현재 시간 형식 지정
-        file_name = f'/{now}.csv'  # 파일 이름 생성
+        Key_path = file_path + f'/{now}.csv'  # 파일 이름 생성
         file = df.to_csv().encode('cp949')  # 데이터프레임을 CSV로 변환 후 인코딩
         
         for try_cnt in range(10):  # 최대 10번 시도
@@ -65,7 +65,7 @@ class DataCrawler:
                 s3_res = self.s3_client.put_object(
                     Body=file,
                     Bucket=self.s3_bucket_name,
-                    Key=file_path + file_name
+                    Key=Key_path
                 )
                 return None  # 성공 시 None 반환
             except Exception as e:
@@ -73,6 +73,29 @@ class DataCrawler:
                 log_message = f"Error saving data to S3 bucket {self.s3_bucket_name}: {e}"
                 continue  # 재시도
         return log_message  # 실패 시 로그 메시지 반환
+    
+    # 가장 최근 파일을 복사하는 메서드
+    def copy_latest_file(self, file_path):
+        now = datetime.now().strftime('%Y-%m-%d_%H') # 현재 시간 형식 지정
+        before = (datetime.now() - timedelta(hours=1)).strftime('%Y-%m-%d_%H')
+        CopySource_path = file_path + f'/{before}.csv'
+        Key_path = file_path + f'/{now}.csv'
+
+        for try_cnt in range(10):  # 최대 10번 시도
+            try:
+                # S3에 파일 업로드
+                s3_res = self.s3_client.copy_object(
+                    Bucket=self.s3_bucket_name,
+                    CopySource=CopySource_path,
+                    Key=Key_path
+                )
+                return None  # 성공 시 None 반환
+            except Exception as e:
+                # 업로드 실패 시 로그 메시지 생성
+                log_message = f"Error saving data to S3 bucket {self.s3_bucket_name}: {e}"
+                continue  # 재시도
+        return log_message  # 실패 시 로그 메시지 반환
+
 
     # 로그를 기록하는 메서드
     def log_result(self, message):
@@ -89,19 +112,31 @@ class KHNPCrawler(DataCrawler):
     def __init__(self, aws_access_key, aws_secret_key, data_portal_key_enc, s3_region):
         # 상위 클래스 초기화 및 S3 버킷 이름 지정
         super().__init__(aws_access_key, aws_secret_key, data_portal_key_enc, 'khnp', s3_region)
-        self.plants = ['WS', 'KR', 'YK', 'UJ', 'SU']  # 발전소 리스트
         self.kinds = ['pwr', 'weather', 'air', 'radiorate', 'inoutwater', 'wastewater']  # 데이터 종류 리스트
+        self.plants = {
+            'pwr': ['WS', 'KR', 'YK', 'UJ', 'SU'],
+            'weather': ['WS', 'KR', 'YK', 'UJ', 'SU'],
+            'air':  ['WS', 'KR', 'YK', 'UJ', 'SU'],
+            'radiorate': ['WS', 'KR', 'YK', 'UJ', 'SU'],
+            'inoutwater': ['WS', 'KR', 'YK', 'UJ'],
+            'wastewater': ['WS', 'KR', 'YK'],
+        }  # 데이터 별 발전소 리스트
         self.base_url = 'http://data.khnp.co.kr/environ/service/realtime/'  # 기본 URL
 
     def fetch_and_process(self, kind, plant):
         params = {'serviceKey': self.data_portal_key_enc, 'genName': plant}  # 요청 파라미터 설정
         df, log_message = self.request_data(self.base_url + kind, params)  # 데이터 요청
+        file_path = f'raw/{kind}/{plant}'  # S3 파일 경로 설정
         if log_message:  # 요청 실패 시
             # 로그 메시지에 추가 정보 포함
             log_message = f'[Kind: {kind} GenName: {plant}] [Requesting data] {log_message}'
             self.log_result(log_message)  # 로그 기록
+            copy_res = self.copy_latest_file(file_path)
+            self.log_result('[Copying data] Copying the most recent data')
+            if copy_res:
+                log_message = f'[Saving to S3] {log_message}'  # 로그 메시지에 추가 정보 포함
+                self.log_result(log_message)  # 로그 기록
         else:
-            file_path = f'{kind}/{plant}'  # S3 파일 경로 설정
             save_to_res = self.save_to_s3(df, file_path)  # 데이터 저장
             if save_to_res:  # 저장 실패 시
                 # 로그 메시지에 추가 정보 포함
@@ -112,7 +147,7 @@ class KHNPCrawler(DataCrawler):
         with ThreadPoolExecutor() as executor:
             futures = []
             for kind in self.kinds:  # 각 데이터 종류에 대해
-                for plant in self.plants:  # 각 발전소에 대해
+                for plant in self.plants[kind]:  # 각 발전소에 대해
                     futures.append(executor.submit(self.fetch_and_process, kind, plant))
             for future in concurrent.futures.as_completed(futures):
                 try:
@@ -131,11 +166,12 @@ class KPXCrawler(DataCrawler):
     def crawl_data(self):
         params = {'serviceKey': self.data_portal_key_enc}  # 요청 파라미터 설정
         df, log_message = self.request_data(self.base_url, params)  # 데이터 요청
+        file_path = 'raw/sukub'  # S3 파일 경로 설정
         if log_message:  # 요청 실패 시
             log_message = f'[Requesting data] {log_message}'  # 로그 메시지에 추가 정보 포함
             self.log_result(log_message)  # 로그 기록
+            
         else:
-            file_path = 'sukub'  # S3 파일 경로 설정
             save_to_res = self.save_to_s3(df, file_path)  # 데이터 저장
             if save_to_res:  # 저장 실패 시
                 log_message = f'[Saving to S3] {log_message}'  # 로그 메시지에 추가 정보 포함
--- transform.py
+++ transform.py
@@ -1,53 +1,141 @@
-import datetime
-
-import pyspark
 from dotenv import load_dotenv
 import os
 from urllib.parse import unquote
 import pyspark
 from pyspark.sql import SparkSession
-import pyspark.sql.functions as F
+from pyspark.sql.types import StringType, StructField, StructType, DoubleType, TimestampType
+from pyspark.sql.functions import split, regexp_extract, monotonically_increasing_id, lit, concat, when, col
 from datetime import datetime
+from functools import reduce
+import boto3
 
-load_dotenv()
+class SparkDataProcessor:
+    def __init__(self):
+        load_dotenv()  # 환경 변수 로드
+        self.AWS_ACCESS_KEY = unquote(os.environ.get('AWS_ACCESS_KEY'))  # AWS 액세스 키 로드
+        self.AWS_SECRET_KEY = unquote(os.environ.get('AWS_SECRET_KEY'))  # AWS 시크릿 키 로드
+        self.S3_REGION = 'ap-northeast-2'  # S3 리전 설정
+        self.spark = self._create_spark_session()  # Spark 세션 생성
+        self.kinds = ['pwr', 'weather', 'air', 'radiorate', 'inoutwater', 'wastewater']  # 데이터 종류 리스트
+        self.plants = {  # 플랜트 종류별 리스트
+            'pwr': ['WS', 'KR', 'YK', 'UJ', 'SU'],
+            'weather': ['WS', 'KR', 'YK', 'UJ', 'SU'],
+            'air': ['WS', 'KR', 'YK', 'UJ', 'SU'],
+            'radiorate': ['WS', 'KR', 'YK', 'UJ', 'SU'],
+            'inoutwater': ['WS', 'KR', 'YK', 'UJ'],
+            'wastewater': ['WS', 'KR', 'YK'],
+        }
+        self.data_schemas = self._define_schemas()  # 데이터 스키마 정의
 
-AWS_ACCESS_KEY = unquote(os.environ.get('AWS_ACCESS_KEY'))
-AWS_SECRET_KEY = unquote(os.environ.get('AWS_SECRET_KEY'))
+    def _create_spark_session(self):
+        conf = pyspark.SparkConf()
+        conf.set('spark.driver.host', '127.0.0.1')  # Spark 드라이버 호스트 설정
+        conf.set('spark.hadoop.fs.s3a.access.key', self.AWS_ACCESS_KEY)  # S3 액세스 키 설정
+        conf.set('spark.hadoop.fs.s3a.secret.key', self.AWS_SECRET_KEY)  # S3 시크릿 키 설정
+        conf.set("spark.jars.packages", 'org.apache.hadoop:hadoop-aws:3.3.4')  # S3 패키지 설정
 
-conf = pyspark.SparkConf()
-conf.set('spark.driver.host', '127.0.0.1')
-conf.set('spark.hadoop.fs.s3a.access.key', AWS_ACCESS_KEY)# Caused by: org.apache.hadoop.fs.s3a.auth.NoAuthWithAWSException: No AWS Credentials provided by TemporaryAWSCredentialsProvider SimpleAWSCredentialsProvider EnvironmentVariableCredentialsProvider IAMInstanceCredentialsProvider : com.amazonaws.SdkClientException: Unable to load AWS credentials from environment variables (AWS_ACCESS_KEY_ID (or AWS_ACCESS_KEY) and AWS_SECRET_KEY (or AWS_SECRET_ACCESS_KEY))
-conf.set('spark.hadoop.fs.s3a.secret.key', AWS_SECRET_KEY)
-conf.set("spark.jars.packages", 'org.apache.hadoop:hadoop-aws:3.3.4') # java.lang.RuntimeException: java.lang.ClassNotFoundException: Class org.apache.hadoop.fs.s3a.S3AFileSystem not found 해결을 위한 구문
+        # Spark 세션 생성 및 반환
+        return SparkSession.builder.config(conf=conf).appName('s3_rw_example').getOrCreate()
 
+    def _define_schemas(self):
+        # 각 데이터 종류별로 스키마 정의 및 반환
+        return {
+            'pwr': StructType([
+                StructField('id', StringType(), True),
+                StructField('name_kor', StringType(), True),
+                StructField('name_eng', StringType(), True),
+                StructField('time', TimestampType(), True),
+                StructField('value', DoubleType(), True)
+            ]),
+            'weather': StructType([
+                StructField('id', StringType(), True),
+                StructField('type', StringType(), True),
+                StructField('name_eng', StringType(), True),
+                StructField('time', TimestampType(), True),
+                StructField('value', DoubleType(), True),
+            ]),
+            'air': StructType([
+                StructField('id', StringType(), True),
+                StructField('name_eng', StringType(), True),
+                StructField('time', TimestampType(), True),
+                StructField('Grade', StringType(), True),
+            ]),
+            'radiorate': StructType([
+                StructField('id', StringType(), True),
+                StructField('position', StringType(), True),
+                StructField('name_eng', StringType(), True),
+                StructField('time', TimestampType(), True),
+                StructField('value', DoubleType(), True),
+            ]),
+            'inoutwater': StructType([
+                StructField('id', StringType(), True),
+                StructField('type', StringType(), True),
+                StructField('name_eng', StringType(), True),
+                StructField('time', TimestampType(), True),
+                StructField('value', DoubleType(), True),
+            ]),
+            'wastewater': StructType([
+                StructField('id', StringType(), True),
+                StructField('expl', StringType(), True),
+                StructField('name_eng', StringType(), True),
+                StructField('time', TimestampType(), True),
+                StructField('value', DoubleType(), True),
+            ])
+        }
 
-spark = SparkSession.builder\
-    .config(conf=conf)\
-    .appName('s3_rw_example')\
-    .getOrCreate()
+    def process_data(self):
+        now = datetime.now().strftime('%Y-%m-%d_%H')  # 현재 시간 형식 지정
+        for kind in self.kinds:
+            dfs = []  # 데이터프레임 리스트 초기화
+            for plant in self.plants[kind]:
+                input_path = f'khnp/raw/{kind}/{plant}/{now}.csv'  # 입력 파일 경로 지정
+                df = self.spark.read.csv(f's3a://{input_path}', header=True, encoding='cp949')  # CSV 파일 읽기
+                df = self._rename_columns(df, kind)  # 컬럼 이름 변경
+                df = self._process_columns(df, kind)  # 컬럼 처리
+                df = df.withColumn('plant', lit(plant))  # 'plant' 컬럼 추가
+                dfs.append(df)  # 데이터프레임 리스트에 추가
+            union_df = reduce(lambda df1, df2: df1.union(df2), dfs)  # 데이터프레임 합치기
+            union_df = union_df.coalesce(1).withColumn("id", monotonically_increasing_id())  # 단일 파티션으로 병합하고 ID 추가
+            union_df.show()  # 결과 출력
+            s3_client = boto3.client(  # S3 클라이언트 생성
+                service_name='s3',
+                aws_access_key_id=self.AWS_ACCESS_KEY,
+                aws_secret_access_key=self.AWS_SECRET_KEY,
+                region_name=self.S3_REGION
+            )
+            output_path = f'khnp/transform/{kind}/{now}/'  # 출력 경로 지정
+            union_df.write.option('header', 'true').parquet(f's3a://{output_path}')  # Parquet 파일로 저장
 
-plants = ['WS', 'KR', 'YK', 'UJ', 'SU']  # 발전소 리스트
-kinds = ['pwr', 'weather', 'air', 'radiorate', 'inoutwater', 'wastewater']  # 데이터 종류 리스트
+    def _rename_columns(self, df, kind):
+        # 스키마에 맞게 데이터프레임의 컬럼 이름 변경
+        schema = self.data_schemas[kind]
+        for old_field, new_field in zip(df.schema.fields, schema.fields):
+            df = df.withColumnRenamed(old_field.name, new_field.name)
+        return df
 
-now = datetime.now().strftime('%Y-%m-%d') + '_16'
+    def _process_columns(self, df, kind):
+        # 데이터 종류에 따라 컬럼 처리
+        if kind == 'pwr':
+            df_split = df.withColumn("name_kor_split", split(df["name_kor"], " "))  # 'name_kor' 컬럼 분할
+            df = df_split.withColumn("plant_num", when(df_split["name_kor_split"].getItem(0).startswith("신"),
+                                                        concat(lit("신"), regexp_extract(df_split["name_kor_split"].getItem(0), r'\d+', 0))) \
+                                                    .otherwise(regexp_extract(df_split["name_kor_split"].getItem(0), r'\d+', 0))) \
+                .withColumn("component", df_split["name_kor_split"].getItem(1)) \
+                .withColumn("measure", df_split["name_kor_split"].getItem(2)) \
+                .drop("name_kor_split").drop('name_kor')
+        elif kind == 'radiorate':
+            df = df.withColumn('location', regexp_extract('position', r"ERMS-(.*)\(MS-\d+\)", 1)) \
+                .drop('position')
+        elif kind == 'inoutwater':
+            df = df.withColumn('type_splited', split(df['type'], '-'))
+            df = df.withColumn('location', df['type_splited'].getItem(0)) \
+                .withColumn('parameter', df['type_splited'].getItem(1)) \
+                .drop('type_splited').drop('type')
+        elif kind == 'wastewater':
+            df = df.withColumn("type", regexp_extract(df["expl"], r"-(.*?)(\(|$)", 1)) \
+                .drop('expl')
+        return df
 
-
-pwr_dfs = []
-for plant in plants:
-    input_path = f'khnp/{kinds[0]}/{plant}/{now}.csv'
-    df = spark.read.csv(f's3a://{input_path}', header=True, encoding='cp949')
-    pwr_dfs.append(df)
-
-
-new_pwr_dfs = []
-
-for i in range(len(pwr_dfs)):
-    new_pwr_df = pwr_dfs[i].selectExpr(
-        '_c0 as gen_id',
-        'expl as name_kor',
-        'name as name_eng',
-        'time as time',
-        'value as value'
-    ).withColumn('plant', F.lit(plants[i]))
-    new_pwr_df.show(truncate=False)
-
+if __name__ == "__main__":
+    processor = SparkDataProcessor()  # SparkDataProcessor 클래스의 인스턴스 생성
+    processor.process_data()  # 데이터 처리 시작
